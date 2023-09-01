40bb74294e8c0bb4e00517f58def6d756b291cf4
AzekaG <azekaggg@gmail.com>
2023-09-01 18:31:57 +0300

    Добавьте файлы проекта.

---
+++ Controller.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace HW_Crytical_Section_Lock_31._08
+{
+    internal class Controller
+    {
+        const int CountPairs = 10;
+        Random rand;
+        public List<int> list;
+        public List<int> list2;
+        FileStream pFile;
+        string path;
+       public  Controller() 
+        {
+            rand = new Random();
+            list = new List<int>();
+            list2 = new List<int>();
+          //  pFile = new FileStream("txtFile.txt", FileMode.OpenOrCreate, FileAccess.Write);
+        }
+
+
+
+
+        public void MakeSomNums()
+        {
+            for (int i = 0; i < CountPairs; i++)
+            {
+                list.Add(rand.Next(0, 100));
+                list2.Add(rand.Next(0, 100));
+            }
+        }
+
+
+        public void CreateSomeNums()
+        {
+
+
+            lock(this)
+            {
+
+                pFile = new FileStream("txtFile.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter streamWriter = new StreamWriter(pFile);
+                streamWriter.WriteLine("Random pairs : ");
+                for (int i = 0; i < CountPairs; i++)
+                {
+                   
+                    streamWriter.WriteLine(list.Last() + "  " + list2.Last());
+                }
+                streamWriter.Close();
+            }
+        }
+
+        public void CreateSummNums()
+        {
+
+            lock (this)
+            {
+                pFile = new FileStream("txtFile.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                pFile.Position = pFile.Length;
+                StreamWriter streamWriter = new StreamWriter(pFile);
+                
+                streamWriter.WriteLine("Summ : ");
+
+                for (int i = 0; i < CountPairs; i++)
+                {
+                    
+                    streamWriter.WriteLine(list.ElementAt(i) + list2.ElementAt(i));
+                }
+                streamWriter.Close();
+            }
+        }
+
+
+        public void CreateProd()
+        {
+            lock (this)
+            {
+                pFile = new FileStream("txtFile.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                pFile.Position = pFile.Length;
+                StreamWriter streamWriter = new StreamWriter(pFile);
+
+                streamWriter.WriteLine("Product  : ");
+
+                for (int i = 0; i < CountPairs; i++)
+                {
+                    streamWriter.WriteLine(list.ElementAt(i) * list2.ElementAt(i));
+                }
+                streamWriter.Close();
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_Crytical_Section_Lock_31._08
+{/*Создайте приложение , испольуещеемеханизм критических секций.
+    Первый поток генерирует несколько пар чисел и сохраняет в файл
+    Второлй поток подсчитывает сумму каждой пары и записывает в файл
+    Третий поток подсчитывает произведение и записывает в файл*/
+    public partial class Form1 : Form
+    {
+
+        Controller controller;
+        public Form1()
+        {
+            InitializeComponent();
+            controller = new Controller();
+            controller.MakeSomNums();
+
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Task ts = new Task(controller.CreateSomeNums);
+            Task ts2 = new Task(controller.CreateSummNums);
+            Task ts3 = new Task(controller.CreateProd);
+            ts.Start();
+            ts2.Start();
+            ts3.Start();
+
+            Task.WaitAll(ts, ts2, ts3);
+            iniListBox();
+
+
+        }
+
+
+        public void iniListBox()
+        {
+            for (int i = 0; i < controller.list.Count; i++)
+            {
+                listBox1.Items.Add($"{controller.list.ElementAt(i)}   {controller.list2.ElementAt(i)}");
+                listBox2.Items.Add($"{controller.list.ElementAt(i) + controller.list2.ElementAt(i)}  ");
+                listBox3.Items.Add($"{controller.list.ElementAt(i) * controller.list2.ElementAt(i)}");
+
+
+            }
+        }
+    }
+}
